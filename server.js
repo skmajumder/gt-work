8dbce0640b646ee35b310bd8d85adf03eb4b9c0c
skmajumder <skmajumder.cse@gmail.com>
2024-02-02 12:18:47 +0600

    Update code

--- server.js
+++ server.js
@@ -33,35 +33,6 @@ db.connect((err) => {
   }
 });
 
-// * A get request to get all users
-app.get("/users", async function (req, res) {
-  const query = "SELECT * FROM users";
-
-  try {
-    // * Execute the query asynchronously
-    const results = await new Promise((resolve, reject) => {
-      db.query(query, (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-
-    // * Send the response to the client with status code 200
-    res.status(200).json(results);
-  } catch (err) {
-    // * An error occurred
-    console.error("Error executing query:", err);
-    res
-      .status(500)
-      .send(
-        "Internal Server Error. The server encountered an unexpected condition to fulfilling the request."
-      );
-  }
-});
-
 // * A post request to submit the form data
 app.post("/submit-form", async function (req, res) {
   const formData = req.body;
